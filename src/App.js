d9936fa4756aacf56178324ff91c2774a11017aa
vaibhav solanki <777vsolanki@gmail.com>
2025-02-05 09:01:46 +0530

    refactor App component and add form handling functionality

--- src/App.js
+++ src/App.js
@@ -12,3 +12,27 @@ const App = () => {
 };
 
 export default App;
+
+
+
+// import React from 'react'
+// const App = () => {
+//   function handleForm (e) {
+//     e.preventDefault()
+//     let form = e.target;
+//     let formData = new FormData(form)
+//     let data = Object.fromEntries(formData.entries())
+//     console.log(data)
+//   }
+//   return (
+//     <div>
+// <form onSubmit={handleForm}> 
+//   <input type='email' name="email" placeholder='email'/>
+//   <input type='password' name="password" placeholder='password'/>
+//   <button type='submit'>submit</button>
+//   </form>
+//     </div>
+//   )
+// }
+
+// export default App
